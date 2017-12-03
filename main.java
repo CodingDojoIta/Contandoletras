4961569f9a51851aa17cf20e9c996ad4b38b6945
Thallison Rhanniel <thallisonrlima@gmail.com>
2017-12-03 17:23:16 -0200

    Update main.java

--- main.java
+++ main.java
@@ -1,14 +1,7 @@
 // authors = 'Thallison', 'Matheus', 'Joab'
-
-//-----------------
-//Este problema foi utilizado em 155 Dojo(s).
-//Se os números de 1 a 5 fossem escritos em palavras: um, dois, três, quatro, cinco, então teríamos utilizado 2 + 4 + 4 + 6 + 5 = 21 letras no total.
-//Se todos os números de 1 até 20 fossem escritos em palavras, quantas letras nós teríamos utilizado?
-
 import java.util.ArrayList;
 
-
-public class cod3 {
+public class codigo {
 
 	public static void main(String[] args) {
         String[] numeros = {"um","dois","três","quatro","cinco","seis","sete","oito","nove","dez","onze","doze","treze","quatorze","quinze",
